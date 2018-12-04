2a97f1a89b79294b3f8f697788f9d77fff762759
Caleb Moses <calebjdmoses@gmail.com>
2018-12-05 09:41:30 +1300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ export RUN=
 
 make tensorboard & make train
 
-zip /output/t2t_data.zip t2t_data/languagemodel_ptb10k/*
-zip /output/t2t_train.zip t2t_train/languagemodel_ptb10k/*
+zip -r /output/t2t_data.zip t2t_data/languagemodel_ptb10k/*
+zip -r /output/t2t_train.zip t2t_train/languagemodel_ptb10k/*
